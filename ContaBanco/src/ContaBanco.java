dc4f6139b80965d4677eeafda1e1c356c88fbd08
Gabriel Lima <gabrielandrr@icloud.com>
2024-08-29 15:09:53 -0300

    Commit

--- ContaBanco/src/ContaBanco.java
+++ ContaBanco/src/ContaBanco.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class ContaBanco {
     public static void main(String[] args) throws Exception {
         
+
+
     }
 }
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,8 @@
 public class ContaTerminal {
     
+    int Numero = 1021;
+    String Agencia = "067-8";
+    String NomeCliente = "Gabriel Lima";
+    double Saldo = 237.48;
+
 }
